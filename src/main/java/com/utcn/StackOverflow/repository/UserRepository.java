8e322778e8aef0f89b2d454550da32cebe705076
iakkeltudor <80157548+iakkeltudor@users.noreply.github.com>
2024-05-22 12:31:27 +0300

    Delete src directory

--- src/main/java/com/utcn/StackOverflow/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.utcn.StackOverflow.repository;
-
-import com.utcn.StackOverflow.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
